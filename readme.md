82079f031d9011c1984e04a085f7298ff10ee10d
ahaoboy <504595380@qq.com>
2024-08-04 20:39:02 +0800

    doc: add npm

--- readme.md
+++ readme.md
@@ -6,6 +6,12 @@ A cross-platform command line utility to watch file system changes.
 cargo binstall chokidar
 ```
 
+## npm
+[chokidar-napi](https://github.com/ahaoboy/chokidar-napi)
+```bash
+npm i @chokidar-napi/chokidar
+```
+
 ## example
 ```bash
 chokidar 'src/**/*.{ts,tsx,json}' -c='pnpm run build' -d 1000
--- src/main.rs
+++ src/main.rs
@@ -5,27 +5,3 @@ fn main() {
     let args = Args::parse();
     chokidar(args);
 }
-
-// use dunce; // Avoids UNC paths on Windows.
-// use std::path::Path;
-// use wax::{Glob, Pattern};
-
-// fn main() {
-//     let path = Path::new("C:/wt/chokidar/src/main.rs");
-//     let directory = Path::new(".");
-//     let (prefix, glob) = Glob::new("../chokidar/src/**").unwrap().partition();
-
-//     glob.walk_with_behavior(directory, || {
-//         let prefix = dunce::canonicalize(directory.join(&prefix)).unwrap();
-//         if dunce::canonicalize(path)
-//             .unwrap()
-//             .strip_prefix(&prefix)
-//             .map(|path| glob.is_match(path))
-//             .unwrap_or(false)
-//         {
-//             println!("{}", "aaa");
-//         }
-//     });
-
-//     println!("{:?}", prefix);
-// }
